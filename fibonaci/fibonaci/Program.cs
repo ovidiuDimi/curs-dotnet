0bae9fd371776ace7bbbef34e2e6c746c7a9337d
opris dimi <ovidiu.dimi21@gmail.com>
2020-03-27 14:39:38 +0200

    fibonaci number

---
+++ fibonaci/fibonaci/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fibonaci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduceti numarul termenilor");
+            int n = int.Parse(Console.ReadLine());
+            int firstNumber = 1, secondNumber = 1, fNumber;
+
+            Console.WriteLine("Number (0) = 1 ");
+            Console.WriteLine("Number (1) = 1 ");
+
+            for (int i = 2; i < n; i++)
+            {
+                fNumber = firstNumber + secondNumber;
+                secondNumber = firstNumber;
+                firstNumber = fNumber;
+
+                Console.WriteLine($"Number ({i}) = {fNumber} " );
+            }
+
+            Console.Read();
+
+        }
+    }
+}
